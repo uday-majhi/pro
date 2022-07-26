5ff534583fd3bc26f9352de2afb4773eaa1dc205
uday-majhi <67256293+uday-majhi@users.noreply.github.com>
2022-07-26 08:08:28 +0545

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+if (!$_SESSION["username"]) {
+    header('location:index.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Login</title>
+</head>
+<body>
+    <div class="outer_container">
+        <h1>Welcome to dashboard</h1>
+        <a href="logout.php">Logout</a>
+    </div>
+</body>
+</html>
